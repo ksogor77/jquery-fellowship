c3a72eb3a8bc5e6ebe4f19b3476ce877273291bb
karrambs <karra.sogor@gmail.com>
2019-09-29 16:25:44 -0700

    Chapter 1 corrections

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -47,34 +47,26 @@ const makeMiddleEarth = () => {
   console.log("Trying to make middle earth.");
 
   // 1. create a section tag with an id of middle-earth
-  $('body').addClass('div id=sections'); 
-  $('body div').addClass(`<section id="middle-earth"></section>`);
-  console.log(`$('middle-earth')`);
+  // $('body').addClass('div id=sections'); 
+  const middle_earth = $.parseHTML('<section id="middle_earth"></section>');
+  console.log(middle_earth);
 
   // 2. append the section to the body of the DOM.
-  $("body div").append(`<section id="middle-earth"></section>`);
-  console.log(`$('middle-earth')`);
+  $('body').append(middle_earth);
+  console.log($('Append middle earth to body'));
   // 3. use a for loop to iterate over the lands array that does the following:
-const createLands = ({lands}) => {
-  createMiddleEarth.forEach((lands) => {
-  const template = `
-  <article id=${lands[i]} class="land">
-    <h1>${lands[i]}</h1>
-  </article>
-  `
-  $('#middle-earth').append(`${lands[i]}`);
-})
-};
-
-console.log(`${lands}`);
-  //   3a. creates an article tag (there should be one for each land when the loop is done)
-
-  //   3b. gives each land article an `id` tag of the corresponding land name
-
-  //   3c. includes an h1 with the name of the land inside each land article
-
-  //   3d. appends each land to the middle-earth section
-
+  lands.forEach(function (arrayItem) {
+    console.log('yeah->' + arrayItem);
+    //   3a. creates an article tag (there should be one for each land when the loop is done)
+    const article = $("<article></article>");
+    console.log('created article: ' + article);
+    //   3b. gives each land article an `id` tag of the corresponding land name
+    article.attr('id',arrayItem); 
+    //   3c. includes an h1 with the name of the land inside each land article
+    article.append($("<h1>" + arrayItem + "</h1>"))
+    //   3d. appends each land to the middle-earth section
+    $('#middle_earth').append(article)
+  });
 };
 
 // COMMIT YOUR WORK
@@ -88,9 +80,13 @@ const makeHobbits = () => {
   console.log('Make hobbits');
 
   // 1. display an unordered list of the hobbits in the shire.
-
+$('#middle-earth #The-Shire').addClass('ul');
+$('#middle-earth #The-Shire ul').addClass('li'*4);
+$('#middle-earth #The-Shire ul li').append('<li>[hobbits[0]]</li>').append('<li>[hobbits[1]]</li>').append('<li>[hobbits[2]]</li>').append('<li>[hobbits[3]]</li>');
+console.log($());
   // 2. give each hobbit a class of "hobbit"
-
+  $('#middle-earth #The-Shire ul li').addClass(className = "hobbit");
+  console.log($('.hobbit'));
   // hint: create a 'ul' outside the loop upon which to append the 'li's
 
   // hint: get 'The-Shire' by using its id
